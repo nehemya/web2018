a4cc43619d667006f33e071e49480e4bcc9eb164
Shaked Eyal <shaked.eyal@gmail.com>
2018-05-25 00:07:37 +0300

    Added reading to the xml file and creating list of countries

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ var xml2js = require('xml2js');
 var parser = new xml2js.Parser();
 var countries = [];
 
-
 fs.readFile('./countries.xml', function (err, data) {
     parser.parseString(data, function (err, result) {
         for (var i = 0; i < result.Countries.Country.length; i++) {
