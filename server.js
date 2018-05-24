1f46760fb81b3723dd613341bb71d0961d350c0d
Shaked Eyal <shaked.eyal@gmail.com>
2018-05-25 00:06:42 +0300

    split the code to different models. added using router

--- server.js
+++ server.js
@@ -7,6 +7,21 @@ var DButilsAzure = require('./DButils');
 var users = require('./Users');
 var poi = require('./POI');
 var port = 3000;
+var fs = require('fs');
+var xml2js = require('xml2js');
+var parser = new xml2js.Parser();
+var countries = [];
+
+
+fs.readFile('./countries.xml', function (err, data) {
+    parser.parseString(data, function (err, result) {
+        for (var i = 0; i < result.Countries.Country.length; i++) {
+            var country = result.Countries.Country[i].Name;
+            countries.push(country[0]);
+        }
+
+    });
+});
 
 
 app.use(bodyParser.urlencoded({extended: false}));
