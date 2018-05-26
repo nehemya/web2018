de58abe3f24a338065696ce85362d87982b15b4b
Shaked Eyal <shaked.eyal@gmail.com>
2018-05-27 00:01:09 +0300

    added saved poi method. authentication now essential to use poi

--- POI.js
+++ POI.js
@@ -12,22 +12,62 @@ router.use(bodyParser.urlencoded({extended: true}));
 router.use(bodyParser.json());
 router.use(cors());
 
+
+function ansToJson(ans)
+{
+    let result=[];
+    for(let i=0;i<ans.length;i++){
+        result[i]= {"PoiName" : ans[i].PoiName , "PoiPic" : ans[i].PoiPic,
+            "Category":ans[i].Category, "Rating":ans[i].Rating};
+    }
+    return result;
+}
+
 router.get('/getAllPOI', function (req,res) {
-    //language=SQLite
-    const query = `SELECT * FROM [POI]`;
-    DButilsAzure.execQuery(query)
-        .then(function (ans) {
-            let result=[];
-            for(let i=0;i<ans.length;i++){
-                result[i]= {"PoiName" : ans[i].PoiName , "PoiPic" : ans[i].PoiPic,
-                    "Category":ans[i].Category, "Rating":ans[i].Rating};
-            }
-            res.send(JSON.stringify(result));
-        })
-        .catch(function (err) {
-            console.log('connection fail')
-        })
-})
+
+    if (undefined === req.decoded)
+    {
+        res.sendStatus(401);
+    }
+    else
+    {
+        //language=SQLite
+        const query = `SELECT * FROM [POI]`;
+        DButilsAzure.execQuery(query)
+            .then(function (ans) {
+                let result = ansToJson(ans);
+                res.send(JSON.stringify(result));
+            })
+            .catch(function (err) {
+                console.log('connection fail')
+            })
+    }
+
+});
+
+router.get('/getSaved', function (req, res) {
+
+    if (undefined === req.decoded)
+    {
+        res.sendStatus(401);
+    }
+    else {
+        const username = req.decoded.payload.UserName;
+        //language=SQLite
+        const query = `SELECT * FROM [POI] WHERE [PoiName] IN (SELECT [poi_name] FROM [SavePOI] WHERE username = '${username}')`;
+        DButilsAzure.execQuery(query)
+            .then(function (ans) {
+                let result = ansToJson(ans);
+                res.send(JSON.stringify(result));
+            })
+            .catch(function (err) {
+                console.log('connection fail')
+            })
+    }
+});
+
+
+
 
 
 
--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(cors());
 /*  middleware  */
 app.use(function(req, res, next){
     console.log(req.method, req.url);
-    let token = req.body.token;
+    let token = req.body.token || req.query.token || req.headers['x-access-token'];
     if (token) {
         jwt.verify(token, secret, function (err, decoded) {
             if (err) {
@@ -46,7 +46,7 @@ app.use(function(req, res, next){
             }
         })
     } else next();
-})
+});
 
 /* configure the route */
 app.use('/Users', users);
