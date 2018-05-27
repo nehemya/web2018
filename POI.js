5485c2c1ffb3e3463fa69c44ecbbca3698de5cfb
Shaked Eyal <shaked.eyal@gmail.com>
2018-05-27 14:44:53 +0300

    fixed getSaved and addSaved pois added delete saved poi added post new review

--- POI.js
+++ POI.js
@@ -35,12 +35,9 @@ function sort(ans) {
 }
 
 
-function getPlace(ans, poiName)
-{
-    for (let i = 0; i < ans.length; i++)
-    {
-        if (ans[i].poi_name !== poiName)
-        {
+function getPlace(ans, poiName) {
+    for (let i = 0; i < ans.length; i++) {
+        if (ans[i].poi_name !== poiName) {
             continue;
         }
         return ans[i].place;
@@ -48,126 +45,142 @@ function getPlace(ans, poiName)
     return -1;
 }
 
+var calculteAvg = function (ans) {
+    return new Promise(function (resolve, reject) {
+
+    })
+};
+
+
+router.use('/', function (req, res, next) {
+    if (undefined === req.decoded) {
+        res.sendStatus(498)
+    }
+    else {
+        next();
+    }
+});
 
 
 router.get("/getAllPOI", function (req, res) {
 
-    if (undefined === req.decoded) {
-        res.sendStatus(498);
-    }
-    else {
-        //language=SQLite
-        const query = `SELECT * FROM [POI]`;
-        DButilsAzure.execQuery(query)
-            .then(function (ans) {
-                let result = ansToJson(ans);
-                res.send(result);
-            })
-            .catch(function (err) {
-                console.log('connection fail')
-            })
-    }
+
+    //language=SQLite
+    const query = `SELECT *
+                   FROM [POI]`;
+    DButilsAzure.execQuery(query)
+        .then(function (ans) {
+            let result = ansToJson(ans);
+            res.send(result);
+        })
+        .catch(function (err) {
+            console.log('connection fail')
+        })
+
 
 });
 
 router.get('/save', function (req, res) {
-    if (undefined === req.decoded) {
-        res.sendStatus(498);
-    }
-    else {
-
-        let pois = [];
-        let sorted = [];
-        const username = req.decoded.payload.UserName;
-        //language=SQLite
-        const query = `SELECT * FROM [POI] WHERE [PoiName] IN (SELECT [poi_name] FROM [SavePOI] WHERE username = '${username}')`;
-
-        DButilsAzure.execQuery(query)
-            .then(function (p) {
-                pois = p;
-            })
-            .catch(function (err) {
-                console.log('connection fail')
-            });
-        //language=SQLite
-        const querySort = `SELECT * FROM [SavePOI] WHERE username = '${username}'`;
-        DButilsAzure.execQuery(querySort)
-            .then(function (ans) {
-                sorted = sort(ans);
-                let result = [];
-                for (let i = 0; i < pois.length; i++)
-                {
-                    let place = getPlace(sorted, pois[i].PoiName);
-                    result[place - 1] = pois[i];
-                }
-                res.send(result)
-
-            })
-
-            .catch(function (err) {
-                console.log('connection fail')
-            });
 
 
-    }
+    let pois = [];
+    let sorted = [];
+    const username = req.decoded.payload.UserName;
+    //language=SQLite
+    const query = `SELECT * FROM [POI] WHERE [PoiName] IN (SELECT [poi_name] FROM [SavePOI] WHERE username = '${username}')`;
+
+    DButilsAzure.execQuery(query)
+        .then(function (p) {
+            pois = p;
+        })
+        .catch(function (err) {
+            console.log('connection fail')
+        });
+    //language=SQLite
+    const querySort = `SELECT * FROM [SavePOI] WHERE username = '${username}'`;
+    DButilsAzure.execQuery(querySort)
+        .then(function (ans) {
+            sorted = sort(ans);
+            let result = [];
+            for (let i = 0; i < pois.length; i++) {
+                let place = getPlace(sorted, pois[i].PoiName);
+                result[place - 1] = pois[i];
+            }
+            res.send(result)
+
+        })
+
+        .catch(function (err) {
+            console.log('connection fail')
+        });
+
+
 });
 
-router.post('/save', function(req, res){
+router.post('/save', function (req, res) {
 
-    if (req.decoded === undefined) {
-        res.sendStatus(498);
-    }
-    else {
-        const username = req.decoded.payload.UserName;
-        let pois = req.body.pois;
-        let promises = [];
 
+    const username = req.decoded.payload.UserName;
+    let pois = req.body.pois;
+    let promises = [];
+
+    //language=SQLite
+    const del = `DELETE FROM [SavePoi] WHERE username = '${username}'`;
+    promises.push(DButilsAzure.execQuery(del));
+    for (let i = 0; i < pois.length; i++) {
+        let poiName = pois[i].poiName;
+        let place = pois[i].place;
         //language=SQLite
-        const del = `DELETE FROM [SavePoi] WHERE username = '${username}'`;
-        promises.push(DButilsAzure.execQuery(del));
-        for (let i = 0; i < pois.length; i++)
-        {
-            let poiName = pois[i].poiName;
-            let place = pois[i].place;
-            //language=SQLite
-            let insert = `INSERT INTO [SavePOI] VALUES ('${username}', '${poiName}', '${place}')`;
-            promises.push(DButilsAzure.execQuery(insert))
-        }
-
-        Promise.all(promises)
-            .then(function () {
-                res.sendStatus(200)
-            })
-            .catch(function (err) {
-                res.send(err.message())
-            })
-
-
+        let insert = `INSERT INTO [SavePOI] VALUES ('${username}', '${poiName}', '${place}')`;
+        promises.push(DButilsAzure.execQuery(insert))
     }
+
+    Promise.all(promises)
+        .then(function () {
+            res.sendStatus(200)
+        })
+        .catch(function (err) {
+            res.send(err.message);
+        })
+
+
 });
 
 
-router.delete('/save', function (req, res){
-    if (undefined === req.decoded)
-    {
-        res.sendStatus(498)
-    }
-    else
-    {
-        const poiName = req.body.PoiName;
-        const username = req.decoded.payload.UserName;
-        //language=SQLite
-        const query = `DELETE FROM [SavePOI] WHERE username = '${username}' AND poi_name = '${poiName}'`;
-        DButilsAzure.execQuery(query)
-            .then(function () {
-                res.sendStatus(200);
-            })
-            .catch(function(err)
-            {
-                res.send(err.message());
-            })
-    }
+router.delete('/save', function (req, res) {
+
+    const poiName = req.body.PoiName;
+    const username = req.decoded.payload.UserName;
+    //language=SQLite
+    const query = `DELETE FROM [SavePOI] WHERE username = '${username}' AND poi_name = '${poiName}'`;
+    DButilsAzure.execQuery(query)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.send(err.message);
+        })
+
 });
+
+router.post('/review', function (req, res) {
+
+    const username = req.decoded.payload.UserName;
+    const poiName = req.body.PoiName;
+    const review = req.body.review;
+
+    //language=SQLite
+    const query = `insert into [PoiReview] values ('${username}', '${poiName}', '${review}')`;
+    DButilsAzure.execQuery(query)
+        .then(function () {
+            res.sendStatus(200)
+        })
+        .catch(function (err) {
+            res.send(err.message);
+        })
+
+});
+
 
 
 module.exports = router;
