ecdd2cebf495942fc68483fb25448df6cf0e00dd
Elior <Nehemya@post.bgu.ac.il>
2018-05-31 18:42:47 +0300

    last version for 3.2

--- users/Users.js
+++ users/Users.js
@@ -12,7 +12,7 @@ router.use(bodyParser.urlencoded({extended: true}));
 router.use(bodyParser.json());
 router.use(cors());
 
-/* adding user */
+/* adding user  */
 router.post('/addUser', function (req, res) {
 
     const username = req.body.UserName;
